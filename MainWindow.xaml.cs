efd7ddd121084e3fe1aab16c0df06b53d343fdde
NitrousOX <poljvasdavid@gmail.com>
2023-05-25 13:31:33 -0700

    Prvi tab

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.RightsManagement;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +20,69 @@ namespace Predmetni_projekat_Formula1
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+            List<Vozac> vozaci = Ucitaj("C:\\Users\\poljv\\Documents\\GitHub\\Predmetni-projekat-Formula1\\vozaci.txt");
+            TxtBox_Pretraga.Text = vozaci[0].ToString();
+        }
+
+        private void Btn_Export_Click(object sender, RoutedEventArgs e)
+        {
+            if ((Radio_CSV.IsChecked == false) && (Radio_XLS.IsChecked == false))
+            {
+                MessageBox.Show("Morate izabrati format");
+            }
+            if(Radio_CSV.IsChecked == true)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+
+
+        List<Vozac> Ucitaj(string putanja)
+        {
+            List<Vozac> vozaci = new List<Vozac>();
+            uint id;
+            string first_Name;
+            string last_Name;
+            string team;
+            string nationality;
+            string chassis_Number;
+            int num_Races;
+            int num_Wins;
+            string picture_path;
+
+            StreamReader reader = File.OpenText(putanja);
+            string line;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] items = line.Split(',');
+                id = uint.Parse(items[0]);
+                first_Name = items[0];
+                last_Name = items[1];
+                team = items[2];
+                nationality = items[3];
+                chassis_Number = items[4];
+                num_Races = int.Parse(items[5]);
+                num_Wins = int.Parse(items[6]);
+                picture_path = "C:\\Users\\poljv\\Documents\\GitHub\\Predmetni-projekat-Formula1\\Slike\\" + items[7];
+
+                vozaci.Add(new Vozac(id, first_Name, last_Name, team, nationality, chassis_Number, num_Races, num_Wins, picture_path));
+            }
+
+            return vozaci;
         }
     }
 }
---
+++ Vozac.cs
@@ -0,0 +1,49 @@
+﻿using Predmetni_projekat_Formula1;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Predmetni_projekat_Formula1
+{
+
+    class Vozac
+    {
+        private uint id;
+        private string first_Name;
+        private string last_Name;
+        private string team;
+        private string nationality;
+        private string chassis_Number;
+        private int num_Races;
+        private int num_Wins;
+        private string picture_path;
+
+        public Vozac(uint ID, string First_Name, string Last_Name, string Team, string Nationality, string Chassis_Number, int Num_Races, int Num_Wins, string Picture_path) {
+            this.id = ID;
+            this.first_Name = First_Name;
+            this.last_Name = Last_Name;
+            this.team = Team;
+            this.nationality = Nationality;
+            this.chassis_Number = Chassis_Number;
+            this.num_Races = Num_Races;
+            this.num_Wins = Num_Wins;
+            this.picture_path = Picture_path;
+        }
+
+        public uint ID { get; set; }
+        public string First_Name { get; set;}
+        public string Last_Name { get; set;}
+        public string Team { get; set;}
+        public string Nationality { get; set;}
+        public string Chassis_Number { get; set;}
+        public int Num_Races { get; set;}
+        public int Num_Wins { get; set;}
+        public string Picture_path { get; set;}
+
+    }
+}
