6aab01cd85948978e6165a1045042ac7072a337a
Mihailo Vojinovic <mihailonvojinovic@gmail.com>
2023-05-25 18:04:14 +0200

    Dodao sliku u trecem tabu

---
+++ Proizvodjac.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Predmetni_projekat_Formula1
+{
+    class Proizvodjac
+    {
+		private string ime;
+		private string zemlja;
+
+		public string Zemlja 
+		{
+			get { return zemlja; }
+			set { zemlja = value; }
+		}
+
+		public string Ime
+		{
+			get { return ime; }
+			set { ime = value; }
+		}
+
+	}
+}
